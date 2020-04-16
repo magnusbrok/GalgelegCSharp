5a4e0eb72488361cbb0d34c4a7ac6b744874811a
magnusbrok <magnus.brok@db-data.dk>
2020-04-16 13:32:14 +0200

    test commit

---
+++ Testere.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalgelegCSharp
+{
+    class Testere
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+}

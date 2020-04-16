dba0873c29015a8e9de40ff70e641d55893726e8
magnusbrok <magnus.brok@db-data.dk>
2020-04-16 13:18:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GalgelegCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

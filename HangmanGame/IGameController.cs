786da62ae63f38d24cc3da97e0bbc6885c024a46
Siff <siff.ravn@gmail.com>
2020-04-22 13:58:33 +0200

    Remane Service Contract
    Co-Authored-By: hellaachari <hellaachari@users.noreply.github.com>

--- HangmanGame/IGameController.cs
+++ HangmanGame/IGameController.cs
@@ -7,9 +7,9 @@ using System.Text;
 
 namespace HangmanGame
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IGameController" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IGameController
     {
         [OperationContract]
         string GetData(int value);
